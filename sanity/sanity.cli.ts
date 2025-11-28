f2f2c8097ca41eee877a759b88bd948e0dadbe15
Dinis1727 <1231727@isep.ipp.pt>
2025-11-28 13:33:15 +0000

    Updated sanity with latest version

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,5 +1,7 @@
 import {defineCliConfig} from 'sanity/cli'
 
+const appId = process.env.SANITY_APP_ID
+
 export default defineCliConfig({
   api: {
     projectId: 'h3s6x795',
@@ -9,7 +11,11 @@ export default defineCliConfig({
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
+     *
+     * When SANITY_APP_ID is not set we turn off auto-updates to avoid the
+     * runtime warning about a missing appId.
      */
-    autoUpdates: true,
+    autoUpdates: Boolean(appId),
+    ...(appId ? {appId} : {}),
   }
 })
